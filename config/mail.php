b6094d6ef55933afdbafa23b315a15d67b2f0402
Tamzid Hossen <tamzidhossen366@gmail.com>
2024-10-07 15:27:13 +0600

    add files

--- config/mail.php
+++ config/mail.php
@@ -111,6 +111,9 @@ return [
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
+
+        // 'address' => env('MAIL_FROM_ADDRESS', 'tamzidbd36@gmail.com'),
+        // 'name' => env('MAIL_FROM_NAME', 'Hello Laravel'),
     ],
 
 ];
